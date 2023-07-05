4d74fb11fc789d6ccfa3f675b2824b2c2136e66a
mandoo1229 <123732776+mandoo1229@users.noreply.github.com>
2023-07-05 15:32:57 +0900

    Refactor : 정령 맞추기

--- src/components/Notice/NoticeView.js
+++ src/components/Notice/NoticeView.js
@@ -1,120 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Header from "../../components/Base/Header";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Header from '../../components/Base/Header';
 import { useNavigate, useLocation } from 'react-router-dom';
 import '../../styles/Notice/NoticeView.css';
-import Moment from "react-moment";
+import Moment from 'react-moment';
 
-const NoticeView = () =>{
-    const location = useLocation();
-    const id = location.state.id;
-    const navigate = useNavigate();
-    const [notice, setNotice] = useState([]);
-    console.log(id);
+const NoticeView = () => {
+  const location = useLocation();
+  const id = location.state.id;
+  const navigate = useNavigate();
+  const [notice, setNotice] = useState([]);
+  console.log(id);
 
-    useEffect(()=> {
-        axios({
-            method: 'GET',
-            url : `/api/notice/view/${id}`,
-            headers: {
-                'Content-Type' : 'application/json'
-            }
-        })
-        .then((res) => {
-            console.log(res.data);
-            setNotice(res.data);          
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    }, [])
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `/api/notice/view/${id}`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('accessToken'),
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setNotice(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  const noticeModifyHandler = () => {
+    navigate('/notice/modify', { state: notice });
+  };
 
-
-
-    const noticeModifyHandler = () => {
-        navigate('/notice/modify', {state:notice})
-    }
-
-    const noticeDeleteHandler = () => {
-        const data = {
-            "id" : notice.id,
-        }
-        axios
-            .post("/api/notice/delete", JSON.stringify(data), {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then((response) => {
-                console.log(response.data);
-                const responseData = response.data;
-    
-                alert("글을 삭제했습니다.")
-                navigate('/notice', {state:responseData})
-            })
-            .then((result) => {
-                console.log(result);
-            })
-            .catch((error) => {
-                console.log(error);
-                alert("글을 삭제하지 못했습니다.")
-            });
-
+  const noticeDeleteHandler = () => {
+    const data = {
+      id: notice.id,
     };
+    axios
+      .post('/api/notice/delete', JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        const responseData = response.data;
 
-    
-    return (
-        <div>
-            <Header />
-        <div align="center" class="notice-view-board">
-        <div class="notice-title">
-                공지사항
-        </div>
+        alert('글을 삭제했습니다.');
+        navigate('/notice', { state: responseData });
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('글을 삭제하지 못했습니다.');
+      });
+  };
+
+  return (
+    <div>
+      <Header />
+      <div align="center" class="notice-view-board">
+        <div class="notice-title">공지사항</div>
         <table class="notice-view-table">
-            <tr class="notice-view-title"> 
-                {/* <td>제목</td> */}
-                <td>{notice.title}</td>
-            </tr>
-            <tr>
-                <div class="view-member-id">
-                    <td class="view-member-id2">작성자</td>
-                    <td>{notice.memberId}</td>
-                </div>
-                <div class="view-created-date">
-                <td class="view-created-date2">작성일시</td>
-                <td class="view-created-date3"><Moment format="YYYY-MM-DD HH:mm:ss">{notice.createdDate}</Moment></td>
-                </div>
-            </tr>
-            {/* <tr>조회수 : {notice.viewCount}</tr><br/> */}
-            <tr class="view-content">
-                <td>{notice.content}</td>
-            </tr>
+          <tr class="notice-view-title">
+            {/* <td>제목</td> */}
+            <td>{notice.title}</td>
+          </tr>
+          <tr>
+            <div class="view-member-id">
+              <td class="view-member-id2">작성자</td>
+              <td>{notice.memberId}</td>
+            </div>
+            <div class="view-created-date">
+              <td class="view-created-date2">작성일시</td>
+              <td class="view-created-date3">
+                <Moment format="YYYY-MM-DD HH:mm:ss">{notice.createdDate}</Moment>
+              </td>
+            </div>
+          </tr>
+          {/* <tr>조회수 : {notice.viewCount}</tr><br/> */}
+          <tr class="view-content">
+            <td>{notice.content}</td>
+          </tr>
         </table>
         <div class="view-buttons">
-            <button
-            type="submit" className="btn btn-primary-view"
-            onClick={() => noticeDeleteHandler()}
-            >삭제하기
-            </button>
+          <button type="submit" className="btn btn-primary-view" onClick={() => noticeDeleteHandler()}>
+            삭제하기
+          </button>
 
-            <button 
-            type="submit" className="btn btn-primary-view"
-            onClick={() => noticeModifyHandler()}
-            >수정하기
-            </button>
+          <button type="submit" className="btn btn-primary-view" onClick={() => noticeModifyHandler()}>
+            수정하기
+          </button>
 
-            <button
-            type="submit" className="btn btn-primary-view"
-            onClick={()=>{
-                navigate('/notice')
+          <button
+            type="submit"
+            className="btn btn-primary-view"
+            onClick={() => {
+              navigate('/notice');
             }}
-            >목록보기</button>
-
+          >
+            목록보기
+          </button>
         </div>
-        </div>
-        </div>
-    );
-}
-    export default NoticeView;
-
+      </div>
+    </div>
+  );
+};
+export default NoticeView;
--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,83 +1,83 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios'
-import Header from "../components/Base/Header";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Header from '../components/Base/Header';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Notice/Notice.css';
-import Moment from "react-moment";
-import Footer from "../components/Base/Footer";
+import Moment from 'react-moment';
+import Footer from '../components/Base/Footer';
 
-const Notice = () =>  {
+const Notice = () => {
+  const [notices, setNotices] = useState([]);
+  const navigate = useNavigate();
 
-    const [notices, setNotices] = useState([]);
-    const navigate = useNavigate();
+  const noticeViewHandler = (id) => {
+    navigate(`/notice/view/${id}`, {
+      state: {
+        id: id,
+      },
+    });
+  };
 
-    const noticeViewHandler = (id) => {
-        
-        navigate(`/notice/view/${id}`,{
-            state : {
-                "id": id
-        }})
-    };
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: '/api/notice',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('accessToken'),
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
 
-    useEffect(() => {
-        axios({
-            method: 'GET',
-            url : "/api/notice",
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((res) => {
-                console.log(res.data);
+        let noticeData = res.data;
 
-                let noticeData = res.data;
+        setNotices(noticeData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-                setNotices(noticeData);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }, []);
-
-    return (
-        <div>
-            <Header />
-        <div align="center" class="notice-board">
-            <div class="notice-title">
-                공지사항
-            </div>
-            <table class="notice-table">
-                <thead>
-                    <tr class="notice-label">
-                        <th>글 번호</th>
-                        <th>제목</th>
-                        <th>작성자</th>
-                        {/* <th>조회수</th> */}
-                        <th>작성일시</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {notices.map((notice) => (
-                        <tr key={notice.id} 
-                            onClick={() => noticeViewHandler(notice.id)}
-                            class="notice-table-row"> 
-                            <th class = "notice-id">{notice.id}</th>
-                            <th>{notice.title}</th>
-                            <th>{notice.memberId}</th>
-                            {/* <th>{notice.viewCount}</th> */}
-                            <th class="notice-createdDate"><Moment format="YYYY-MM-DD HH:mm:ss">{notice.createdDate}</Moment></th>
-                        </tr>
-                    ))}
-                </tbody>
-            </table> <br />
-            <Link to="write"><button type="button" class="btn btn-primary-notice">글쓰기</button></Link>
-        </div>    
-        <Footer />
-        </div>
-
-
-    );
-}
+  return (
+    <div>
+      <Header />
+      <div align="center" class="notice-board">
+        <div class="notice-title">공지사항</div>
+        <table class="notice-table">
+          <thead>
+            <tr class="notice-label">
+              <th>글 번호</th>
+              <th>제목</th>
+              <th>작성자</th>
+              {/* <th>조회수</th> */}
+              <th>작성일시</th>
+            </tr>
+          </thead>
+          <tbody>
+            {notices.map((notice) => (
+              <tr key={notice.id} onClick={() => noticeViewHandler(notice.id)} class="notice-table-row">
+                <th class="notice-id">{notice.id}</th>
+                <th>{notice.title}</th>
+                <th>{notice.memberId}</th>
+                {/* <th>{notice.viewCount}</th> */}
+                <th class="notice-createdDate">
+                  <Moment format="YYYY-MM-DD HH:mm:ss">{notice.createdDate}</Moment>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>{' '}
+        <br />
+        <Link to="write">
+          <button type="button" class="btn btn-primary-notice">
+            글쓰기
+          </button>
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 export default Notice;
-
