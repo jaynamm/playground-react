7f6913c94185aa80ff568efe8001fe21cb2b8e86
mandoo1229 <123732776+mandoo1229@users.noreply.github.com>
2023-07-09 22:35:02 +0900

    Style : 디자인부분을 수정하였습니다.

--- src/components/Mypage/MyPage.js
+++ src/components/Mypage/MyPage.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from '../Token/Interceptor';
+
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Footer from '../Base/Footer';
-import Header from '../Base/Header';
-// import { useNavigate } from 'react-router-dom';
-import { Avatar } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
-import '../../styles/Mypage.css';
-import axios from '../Token/Interceptor';
-import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+import Header from '../Base/Header';
+import '../../styles/Mypage.css';
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -46,30 +51,19 @@ function a11yProps(index) {
 }
 
 const MyPage = () => {
-  // const location = useLocation();
-  // const member = location.state; // notice 가 mypage로 바뀜
-  // const navigate = useNavigate();
   const [value, setValue] = React.useState(0);
   const [mypage, setMypage] = useState([]);
   const [folowMyPage, setFollowMyPage] = useState([]);
   const [myPageFeedDtoList, setMyPageFeedDtoList] = useState([]);
   const [myPageQuestionDtoList, setMyPageQuestionDtoList] = useState([]);
+  const [expanded, setExpanded] = React.useState(false);
 
-  // const createdDateProfile = new Date(mypage.createdDate);
-  // const creactedDatePro = createdDateProfile.toLocaleDateString('ko-KR', {
-  //   year: 'numeric',
-  //   month: '2-digit',
-  //   day: '2-digit',
-  // });
-
-  //refreshTokne을 다시 받습니다.
   useEffect(() => {
     axios({
       method: 'GET',
       url: '/api/mypage',
       headers: {
         'Content-Type': 'application/json',
-        // Authorization: localStorage.getItem('accessToken'),
       },
     })
       .then((response) => {
@@ -88,104 +82,105 @@ const MyPage = () => {
     setValue(newValue);
   };
 
-  // const myPageModifyHandler = () => {
-  //   navigate('/mypage/modify', { state: member });
-  // };
+  const handleChanges = (panelId) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panelId : null);
+  };
 
   return (
     <div>
       <Header />
-      <div className="title">마이 페이지</div>
-      <div className="container" id="profile">
-        <div className="profile_image">
-          <Avatar src="/broken-image.jpg" id="image" />
-        </div>
-        <div className="table profile_info">
-          <table>
-            <tbody>
-              {mypage && folowMyPage && (
-                <tr id="firstrow">
-                  <td>
-                    <div>
-                      <p>이름 : {mypage.name} </p>
-                      <p>이메일 : {mypage.email} </p>
-                      <p>교육과정 : {mypage.curriculum} </p>
-                      <p>가입날짜 : {folowMyPage.created} </p>
-                      <p>팔로잉 : {folowMyPage.followingCount}</p>
-                      <p>팔로워 : {folowMyPage.followerCount}</p>
-                    </div>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      <Box sx={{ width: '116%', display: 'flex', justifyContent: 'center', marginTop: '30px', marginBottom: '500px' }}>
-        <Box>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-            sx={{ display: 'flex', justifyContent: 'center' }}
-          >
-            <Tab sx={{ width: '250px' }} label="프로필" {...a11yProps(0)} />
-            <Tab sx={{ width: '250px' }} label="내가 쓴 글" {...a11yProps(1)} />
-            <Tab sx={{ width: '250px' }} label="내가 쓴 댓글" {...a11yProps(2)} />
-          </Tabs>
-          <TabPanel value={value} index={0}>
-            <div>
-              <Box sx={{ width: '1000px', display: 'flex', justifyContent: 'center' }}>
-                <Button>
+      <div>
+        <Box sx={{ display: 'flex', justifyContent: 'center', marginRight: '100px' }}>
+          {mypage && folowMyPage && (
+            <Card sx={{ width: 260, height: 300, minWidth: 275, marginTop: '80px', backgroundColor: '#EDF4FF' }}>
+              <CardContent>
+                <Typography variant="h5" component="div" sx={{ textAlign: 'center', marginBottom: '10px' }}>
+                  {mypage.name}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  이메일 : {mypage.email}
+                  <br />
+                  팔로잉 : {folowMyPage.followingCount}
+                  <br />
+                  팔로워 : {folowMyPage.followerCount}
+                </Typography>
+                <Typography variant="body2">{mypage.myskill}</Typography>
+              </CardContent>
+              <CardActions sx={{ justifyContent: 'center' }}>
+                <Button variant="contained">
                   <Link to="/Mypage/MySkill">스킬 추가</Link>
                 </Button>
-              </Box>
-            </div>
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            <div>
-              <Box sx={{ width: '1000px', justifyContent: 'center' }}>
-                {myPageFeedDtoList.map((feed) => (
-                  <div key={feed.id}>
-                    <table>
-                      <tbody>
-                        <td>
-                          <tr>
-                            <th>내용 : {feed.content} </th>
-                            <th>작성일 : {feed.createdDate} </th>
-                          </tr>
-                        </td>
-                      </tbody>
-                    </table>
-                  </div>
-                ))}
-              </Box>
-            </div>
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            <div>
-              <Box sx={{ width: '1000px', justifyContent: 'center' }}>
-                {myPageQuestionDtoList.map((question) => (
-                  <div key={question.id}>
-                    <table>
-                      <tbody>
-                        <td>
-                          <tr>
-                            <th>내용 : {question.content}</th>
-                            <th>작성일 : {question.createdDate} </th>
-                          </tr>
-                        </td>
-                      </tbody>
-                    </table>
-                  </div>
-                ))}
-              </Box>
-            </div>
-          </TabPanel>
+              </CardActions>
+            </Card>
+          )}
+
+          <Box sx={{ marginLeft: '80px' }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+              sx={{ display: 'flex', justifyContent: 'center', marginTop: '5%' }}
+            >
+              <Tab sx={{ width: '250px' }} label="내가 쓴 글" {...a11yProps(1)} />
+              <Tab sx={{ width: '250px' }} label="내가 쓴 댓글" {...a11yProps(2)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+              <div>
+                <Box sx={{ justifyContent: 'center' }}>
+                  {myPageFeedDtoList.map((feed) => (
+                    <div key={feed.id}>
+                      <Accordion
+                        expanded={expanded === feed.id}
+                        onChange={handleChanges(feed.id)}
+                        sx={{ marginBottom: '1%' }}
+                      >
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls={`panel-${feed.id}-content`}
+                          id={`panel-${feed.id}-header`}
+                        >
+                          <Typography sx={{ width: '33%', flexShrink: 0 }}>{feed.nickname}</Typography>
+                          <Typography sx={{ color: 'text.secondary' }}>{feed.createdDate}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>{feed.content}</Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                    </div>
+                  ))}
+                </Box>
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <div>
+                <Box sx={{ justifyContent: 'center' }}>
+                  {myPageQuestionDtoList.map((question) => (
+                    <div key={question.id}>
+                      <Accordion
+                        expanded={expanded === question.id}
+                        onChange={handleChanges(question.id)}
+                        sx={{ marginBottom: '1%' }}
+                      >
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls={`panel-${question.id}-content`}
+                          id={`panel-${question.id}-header`}
+                        >
+                          <Typography sx={{ width: '33%', flexShrink: 0 }}>{question.title}</Typography>
+                          <Typography sx={{ color: 'text.secondary' }}>{question.createdDate}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>{question.content}</Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                    </div>
+                  ))}
+                </Box>
+              </div>
+            </TabPanel>
+          </Box>
         </Box>
-      </Box>
-      <Footer />
+      </div>
     </div>
   );
 };
