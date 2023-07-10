6dae18ecb0c9a27bd1ca04f4efbf4eb74d47e629
KYHH <kyhh93@naver.com>
2023-07-10 10:18:04 +0900

    Fix : axios backend 와 연결해결

--- src/components/Token/Interceptor.js
+++ src/components/Token/Interceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
 axios.interceptors.request.use(
   function (request) {
+    console.log("send request")
     request.headers['Authorization'] = localStorage.getItem('accessToken');
     return request;
   },
@@ -9,30 +9,28 @@ axios.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
 axios.interceptors.response.use(
   function (response) {
     return response;
   },
   async (error) => {
     const { response, config } = error;
-    if (response && response.status === 401) {
-      const { data } = await axios.get('/api', {
-        baseURL: 'http://localhost:3000',
-        params: {
-          //refresh Token으로 재발급 요청 401에러가 뜨면?
-          token: localStorage.getItem('refreshToken'),
+    console.log("error");
+    if (response.status === 401) {
+      const { data } = await axios.post('/api/refreshtoken', null, {
+        headers: {
+          'refresh-token': localStorage.getItem('refreshToken'),
         },
       });
-      const { accessToken } = data;
+      console.log(data);
       //액세스 토큰을 localStorege에 저장
-      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('accessToken', data);
       // 새 액세스 토큰을 header에 저장
-      config.headers['Authorization'] = accessToken;
+      config.headers['Authorization'] = data;
       // 재요청
       return await axios(config);
     }
     return Promise.reject(error);
   }
 );
-export default axios;
+export default axios; 
