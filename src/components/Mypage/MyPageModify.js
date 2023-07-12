7c106fa387b0070e535c90a7c2f8ddff8f42b49a
mandoo1229 <123732776+mandoo1229@users.noreply.github.com>
2023-07-12 16:05:47 +0900

    Feat : 비밀번호 변경 기능을 추가하였습니다.

--- src/components/Mypage/MyPageModify.js
+++ src/components/Mypage/MyPageModify.js
@@ -6,40 +6,38 @@ import Header from '../Base/Header';
 import { useNavigate } from 'react-router-dom';
 
 const MyPageModify = () => {
-  const [userPassword, setPassword] = useState('');
+  const [userPassword, setUserPassword] = useState('');
   const [userNewPassword, setUserNewPassword] = useState('');
   const [userNewPasswordCheck, setUserNewPasswordCheck] = useState('');
   const navigate = useNavigate();
 
   const passwordChangeHandler = () => {
-    const regl = /^[A-Za-z0-9]{8,20}$/;
-    // 패스워드랑 패스워드 확인이 일치하는지 검증
-    if (userNewPassword !== userNewPasswordCheck) {
-      alert('패스워드 일치하지 않습니다.');
-      return;
-    }
-    if (userPassword.match(regl) === null) {
-      alert('비밀번호 형식에 맞지 않습니다.');
-      return;
-    }
+    // const regl = /^[A-Za-z0-9]{8,20}$/;
+    // // 패스워드랑 패스워드 확인이 일치하는지 검증
+    // if (userNewPassword !== userNewPasswordCheck) {
+    //   alert('패스워드가 일치하지 않습니다.');
+    //   return;
+    // }
+    // if (!regl.test(userNewPassword)) {
+    //   alert('비밀번호 형식이 올바르지 않습니다.');
+    //   return;
+    // }
 
-    const data = {
-      userNewPassword: userNewPassword,
-    };
-
-    axios //입력한 데이터를 받음.
-      .post('/api/member/signup', data)
+    axios // 입력한 데이터를 전송
+      .post('/api/member/password', { password: userPassword })
       .then((response) => {
-        console.log(response.data);
-
-        alert('회원가입에 성공하였습니다.');
-
-        navigate('/signin');
-      }) //성공했을시 response를 받아옴.
+        console.log(response);
+        if (response.code.includes('ERR_BAD_RESPONSE')) {
+          alert('');
+          setUserPassword(false);
+        }
+        alert('비밀번호 변경에 성공하였습니다.');
+        navigate('/mypage');
+      })
       .catch((error) => {
         console.log(error);
-        alert('회원가입에 실패하였습니다.');
-      }); //실패했을 시 error를 발생함.
+        alert('비밀번호 변경에 실패하였습니다.');
+      });
   };
 
   return (
@@ -55,22 +53,22 @@ const MyPageModify = () => {
                 fullWidth
                 label="현재 비밀번호"
                 name="password"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setUserPassword(e.target.value)}
                 type="password"
                 value={userPassword}
               />
               <TextField
                 fullWidth
                 label="새 비밀번호"
-                name="confirm"
+                name="newPassword"
                 onChange={(e) => setUserNewPassword(e.target.value)}
                 type="password"
                 value={userNewPassword}
               />
               <TextField
                 fullWidth
-                label="새 비밀번호"
-                name="confirm"
+                label="새 비밀번호 확인"
+                name="newPasswordCheck"
                 onChange={(e) => setUserNewPasswordCheck(e.target.value)}
                 type="password"
                 value={userNewPasswordCheck}
@@ -79,8 +77,8 @@ const MyPageModify = () => {
           </CardContent>
           <Divider />
           <CardActions sx={{ justifyContent: 'center', marginTop: '5%' }}>
-            <Button variant="contained" onClick={() => passwordChangeHandler()}>
-              <Link to="/mypage">비밀번호 변경하기</Link>
+            <Button variant="contained" onClick={passwordChangeHandler}>
+              비밀번호 변경하기
             </Button>
             <Button variant="contained" color="error">
               <Link to="/mypage">비밀번호 변경 취소하기</Link>
