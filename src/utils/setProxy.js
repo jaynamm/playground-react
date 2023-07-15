baf3b3bf77c239ad7be52cb41bc9ae3ba963496d
jaynamm <polky0421@gmail.com>
2023-07-15 19:38:23 +0900

    Deploy: pm2 설치 및 배포 설정

--- src/utils/setProxy.js
+++ src/utils/setProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(createProxyMiddleware('/api', { // /api 로 시작하는 url 에 대해서 프록시 설정
-            target: 'http://localhost:8080', // 비즈니스 서버 URL 설정
+            target: 'http://54.180.81.200:8080', // 비즈니스 서버 URL 설정
             changeOrigin: true
         })
     );
